3d6e313cd34487bd8fa40440d04960590e6bda97
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-06-28 16:13:14 +0000

    variables and variables  naming rules

---
+++ variable.js
@@ -0,0 +1,11 @@
+let a = 8;
+
+var b = 10;
+
+const c = 12;
+
+console.log(a);
+
+console.log(b);
+
+console.log(c);
---
+++ variablenamerule.js
@@ -0,0 +1,31 @@
+let fisrt_name = "mohit";
+
+let contact = 9898989898;
+
+// let first-name = "mohit sahu"  // wrongs
+
+let first$name = "rohit";
+
+let _price = 100;
+
+let $price = 200;
+
+let price = 300;
+
+// let 4price = 400; // wrongs
+
+var price4 = 400;
+
+console.log(fisrt_name);
+
+console.log(contact);
+
+console.log(first$name);
+
+console.log(_price);
+
+console.log($price);
+
+console.log(price);
+
+console.log(price4);
