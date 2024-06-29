0edd5ee6eabe889cfbb05ca1294c77b482c17644
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-06-29 03:54:58 +0000

    added Arithmatic operators

---
+++ operators/Arithmatic.operators.js
@@ -0,0 +1,15 @@
+let x = 3;
+ 
+let y = 2;
+
+console.log(x+y); // addition
+
+console.log(x-y); // subtraction
+
+console.log(x*y); // multiplication
+
+console.log(x/y); // division  --  this will give quotient
+
+console.log(x%y); // modulus --  this will  give remender
+
+console.log(x**y); // exponent -- this will give power
