a6572c8a32aa9335e39b8f1c3a0d148618d3d400
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-06-29 04:14:56 +0000

    assignment Operators

---
+++ operators/Assignmentoperators.js
@@ -0,0 +1,27 @@
+// this operator help us to assign a value to a  variable
+
+let x = 5; // equal to operator
+
+x += 10; // this is a shorthand operator it simple  mean is that  ( x = x + 10)
+
+console.log(x);
+
+x -= 10; // this is a shorthand operator it simple  mean is that  ( x = x - 10)
+
+console.log(x);
+
+x /= 10; // this is a shorthand operator it simple  mean is that  ( x = x / 10)
+
+console.log(x);
+
+x *= 10; // this is a shorthand operator it simple  mean is that  ( x = x * 10)
+
+console.log(x);
+
+x %= 10; // this is a shorthand operator it simple  mean is that  ( x = x % 10)
+
+console.log(x);
+
+x **= 10; // this is a shorthand operator it simple  mean is that  ( x = x ** 10)
+
+console.log(x);
