c6f9f62e8808b7f374508949c2a897a7e284b4bf
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-06-29 07:26:49 +0000

    conditionals javascript

---
+++ Conditional/ifelsedemo.js
@@ -0,0 +1,27 @@
+console.log("starting...");
+
+//example 1
+let isUserSigned = true;
+
+if (isUserSigned == true && isUserPaid == true) {
+    console.log("Show the dashbord");
+}
+else if (isUserSigned == true && isUserPaid == false) {
+    console.log("Show the payment page");
+}
+else{
+    console.log("Show the signup page");
+}
+
+
+
+// example 1
+let isUserSignedd = false;
+
+if (isUserSignedd == true) {
+    console.log("user is signed in");
+} else {
+    console.log("user is not signed in");
+}
+
+console.log("end.");
---
+++ Conditional/nested.js
@@ -0,0 +1,15 @@
+let isUserSubscribe = true;
+let isUserHasGatepay = false;
+let isUserHasSonypay = true;
+
+if(isUserSubscribe){
+    if(isUserHasGatepay){
+        console.log("show the jhon wick");
+    }
+    else if(isUserHasSonypay){
+        console.log("show the batman");
+    }
+    else{
+        console.log("show the panchayat");
+    }
+}
