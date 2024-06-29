4007a82563acf08c10f36ea16656d91352afaabe
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-06-29 06:06:46 +0000

    Short-circuit evaluation in javascript.

---
+++ operators/SortCircuit.js
@@ -0,0 +1,26 @@
+// Truthy and falsy value in javascript.
+
+// AND 
+// here are two conditions
+// 1. -->  if first is true then return second value
+// 2. -->  if first is false then return first value
+
+
+console.log(10 && 30);  // return 30
+
+console.log(0 && 30); // return 0
+
+console.log(4 && null); // return null
+
+
+// OR 
+// here are two conditions
+// 1. -->  if first is true then return first value
+// 2. -->  if first is false then return second value
+
+
+console.log(10 || 30);  // return 10
+
+console.log(0 || 30); // return 30
+
+console.log(4 || null); // return 4
