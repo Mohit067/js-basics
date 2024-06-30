8c4ee75b081dc6bc283bf6d28b2a5b714efaee05
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-06-30 17:12:21 +0000

    Loop added

---
+++ Loops/Fordemo.js
@@ -0,0 +1,5 @@
+// for loop ( initialization, condition, increment)
+for(let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+console.log("End");
---
+++ Loops/Whiledemo.js
@@ -0,0 +1,7 @@
+let i = 1; // Looping variable
+
+while (i < 10) { // Condition
+    console.log(i);
+    i += 1;
+}
+console.log("End");
