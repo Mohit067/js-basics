d5d321bd871a8036b9a3006820f14aacb4f87920
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-06-30 04:15:07 +0000

    Bitwise Operator added

---
+++ operators/BitwiseOperator.js
@@ -0,0 +1,23 @@
+const a = 5;
+
+const b = 4;
+
+// Bitwise AND
+console.log(a & b);
+
+// Bitwise OR
+console.log(a | b);
+
+// Bitwise XOR
+console.log(a ^ b);
+
+// Bitwise left shift
+console.log(a << 2);
+
+console.log(1 << 4);
+
+// Bitwise right shift
+console.log(a >> 2);
+
+console.log(27 >> 2);
+
