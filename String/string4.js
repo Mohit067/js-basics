2256a6c8b7d388eea89b7f0841f5d65f9ad1d418
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-07-03 07:57:36 +0000

    string basics added

---
+++ String/string4.js
@@ -0,0 +1,16 @@
+// here we add two or more string
+
+let x = "abc";
+let y = 'def';
+let z = `ghy`;
+
+console.log(x + y + z);//one way
+
+let result = x + y + z; // another way
+console.log(result);    
+
+console.log(z.concat("-", "abbbb")); // concatination 
+// pari of key value
+// first " here we write what we want saparate in between those string" , " here is another  string"
+
+
