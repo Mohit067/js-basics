5b6857c1cdc07571ab11a57d079cfd95ab853d68
Mohit sahu <mohitsahu60067@gmail.com>
2024-09-23 23:05:19 +0530

    switch added

---
+++ Conditional/Switch.js
@@ -0,0 +1,18 @@
+let months_quarter = 6;
+
+switch(months_quarter){
+    case 1:
+        console.log("january , fabruary , march");
+        break;
+    case 2:
+        console.log("april, may, june");
+        break;
+    case 3:
+        console.log("july , august , september");
+        break;
+    case 4:
+        console.log("october november december");
+        break;
+    default:
+        console.log(`Sorry, we are out of ${months_quarter}.`);
+}
